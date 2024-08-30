0f280386323c01318ea738c4c317dbba21a12888
jacklavelle286 <lavellej286@gmail.com>
2024-08-30 22:24:39 +0100

    adding route table

--- main.tf
+++ main.tf
@@ -1,3 +1,5 @@
+# vpc and subnets
+
 module "app_vpc" {
   source = "./modules/vpc"
   cidr_block = "10.0.0.0/16"
@@ -9,4 +11,19 @@ module "private_subnet_1" {
   subnet_cidr_block = "10.0.1.0/24"
   subnet_name = "Private-1-App-VPC"
   vpc_id = module.app_vpc.vpc_id
+}
+
+
+module "private_subnet_1" {
+  source = "./modules/subnet"
+  subnet_cidr_block = "10.0.2.0/24"
+  subnet_name = "Private-2-App-VPC"
+  vpc_id = module.app_vpc.vpc_id
+}
+
+module "main_route_table" {
+  source = "./modules/route_table"
+  vpc_id = module.app_vpc.vpc_id
+  cidr_block = module.app_vpc.vpc_cidr_block
+  gateway_id = "local"
 }
--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,3 +1,7 @@
 output "vpc_id" {
   value = aws_vpc.this.id
 }
+
+output "vpc_cidr_bloc" {
+  value = aws_vpc.this.cidr_block
+}
