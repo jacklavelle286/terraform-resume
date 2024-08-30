63419aba242839c314cc411553d9da4a18f349e6
jacklavelle286 <lavellej286@gmail.com>
2024-08-30 16:45:47 +0100

    adding backend

--- providers.tf
+++ providers.tf
@@ -1,3 +1,11 @@
 provider "aws" {
  region = "us-east-1"
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "tfbackendstate20240930"
+    key            = "terraform.tfstate"
+    region         = "us-east-1" 
+  }
+}
