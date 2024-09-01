52ad58585c60d9411bee735d87abf048beb0f73e
jacklavelle286 <lavellej286@gmail.com>
2024-09-01 20:18:01 +0100

    adding pipeline

--- modules/security_group_rules/ingress/main.tf
+++ modules/security_group_rules/ingress/main.tf
@@ -4,5 +4,5 @@ resource "aws_vpc_security_group_ingress_rule" "this" {
   cidr_ipv4 = var.cidr_ipv4
   from_port = var.from_port
   to_port = var.to_port
- 
+  referenced_security_group_id = var.inbound_sg_id
 }
