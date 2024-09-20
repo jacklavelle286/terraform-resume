428097a725ae9d3f47717af2836653499c817086
jacklavelle286 <lavellej286@gmail.com>
2024-09-20 22:24:08 +0100

    adding view counter

--- modules/dynamodb_table/main.tf
+++ modules/dynamodb_table/main.tf
@@ -1,6 +1,8 @@
 resource "aws_dynamodb_table" "this" {
   name         = var.table_name
   billing_mode = "PROVISIONED"
+  read_capacity = 1
+  write_capacity = 1
 
   dynamic "attribute" {
     for_each = var.dynamodb_attributes
