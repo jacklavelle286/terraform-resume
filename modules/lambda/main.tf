652aa9fa21487c553566064adabbd6a9701ef78e
jacklavelle286 <lavellej286@gmail.com>
2024-09-20 22:02:14 +0100

    adding view counter

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -22,30 +22,6 @@ resource "aws_lambda_function" "this" {
   }
 }
 
-data "archive_file" "lambda_zip" {
-  type        = "zip"
-  source_dir  = var.source_dir
-  output_path = "${path.module}/lambda_function.zip"
-}
-
-locals {
-  env_variables = {
-    for key, value in var.env_map : key => value
-  }
-}
-
-resource "aws_lambda_function" "this" {
-  function_name = var.function_name
-  role          = var.role
-  handler       = var.handler
-  runtime       = var.runtime
-  filename      = data.archive_file.lambda_zip.output_path
-
-  environment {
-    variables = local.env_variables
-  }
-}
-
 resource "aws_lambda_function_url" "this" {
   count = var.create_function_url ? 1 : 0
 
